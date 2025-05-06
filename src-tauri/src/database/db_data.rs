b4f1eb7f3e10ba3df6e020193910314d6cd3d729
guapi-exe <3051016381@qq.com>
2025-05-06 22:15:48 +0800

    推送部分资源文件

---
+++ src-tauri/src/database/db_data.rs
@@ -0,0 +1,52 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Schematic {
+    pub id: i64,
+    pub name: String,
+    pub description: String,
+    #[serde(default = "default_type")]
+    pub schematic_type: i32,
+    #[serde(default = "default_type")]
+    pub sub_type: i32,
+    #[serde(default)]
+    pub is_deleted: bool,
+    pub sizes: String,
+    pub user: String,
+    pub is_upload: bool,
+    pub version: i32,
+    pub game_version: String,
+    pub version_list: String,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct PaginatedResponse<T> {
+    pub data: Vec<T>,
+    pub page: i32,
+    pub page_size: i32,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LogEntry {
+    pub level: String,
+    pub target: String,
+    pub message: String,
+    pub context: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserData {
+    pub id: i64,
+    pub nickname: String,
+    pub avatar: String,
+    pub qq: String,
+    pub access_token: String,
+    pub openid: String,
+    pub schematics: i32,
+    pub cloud: i32,
+}
+
+fn default_type() -> i32 {
+    -1
+}
---
+++ src/views/settingViews.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import {isLeaving, navigationGuard} from "../modules/navigation.ts";
+import {opacity} from "../modules/theme.ts";
+import {onBeforeRouteLeave} from "vue-router";
+import {ref} from "vue";
+const autoUpdateEnabled = ref(true);
+const updateSources = ref([
+  'https://official.update.com',
+  'https://mirror1.update.com',
+  'https://mirror2.update.com'
+]);
+const selectedSource = ref('https://official.update.com');
+onBeforeRouteLeave(navigationGuard)
+</script>
+
+<template class="page-wrapper">
+  <v-row no-gutters
+         class="mb-4 animate-row"
+         :class="{ 'animate-row-out': isLeaving }"
+  >
+    <v-col cols="12" class="mb-4">
+      <v-card class="mx-auto overflow-auto h-auto v-theme--custom text-primary" :style="{ '--surface-alpha': opacity }" elevation="4" >
+        <v-toolbar density="compact" class="bg-blue-grey-lighten-5 pa-4">
+          <v-toolbar-title>
+            <v-icon icon="mdi-cog-outline" class="mr-2"></v-icon>
+            <span class="text-h5">设置</span>
+          </v-toolbar-title>
+        </v-toolbar>
+
+      </v-card>
+    </v-col>
+    <v-col cols="12" class="mb-4">
+      <v-card class="mx-auto" :style="{ '--surface-alpha': opacity }" elevation="4" hover>
+        <v-toolbar density="compact" class="pa-2">
+          <v-toolbar-title>
+            <v-icon icon="mdi-update" class="mr-2"></v-icon>
+            <span class="text-h7">更新设置</span>
+          </v-toolbar-title>
+        </v-toolbar>
+
+        <v-card-text class="pa-4">
+          <v-list class="pa-4" density="comfortable">
+            <v-list-item>
+              <template #prepend>
+                <v-icon icon="mdi-autorenew" class="mr-2"></v-icon>
+              </template>
+              <v-list-item-title>启用自动更新</v-list-item-title>
+              <template #append>
+                <v-switch
+                    v-model="autoUpdateEnabled"
+                    color="primary"
+                ></v-switch>
+              </template>
+            </v-list-item>
+
+            <v-list-item>
+              <template #prepend>
+                <v-icon icon="mdi-database-cog" class="mr-2"></v-icon>
+              </template>
+              <v-list-item-title>更新源设置</v-list-item-title>
+              <template #append>
+                <v-combobox
+                    v-model="selectedSource"
+                    :items="updateSources"
+                    label="选择或输入更新源"
+                    variant="outlined"
+                    density="compact"
+                    class="mt-2"
+                    style="width: 800px;"
+                >
+                  <template #no-data>
+                    <v-list-item>
+                      <v-list-item-title>
+                        输入有效的HTTP地址
+                      </v-list-item-title>
+                    </v-list-item>
+                  </template>
+                </v-combobox>
+              </template>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+
+</style>
