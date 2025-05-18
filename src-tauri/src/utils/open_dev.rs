3aabd759b4e1feaa666d64af2e501ab8bc7373b6
guapi-exe <3051016381@qq.com>
2025-05-18 17:39:31 +0800

    修复调试模式-v0.4.5

--- src-tauri/src/utils/open_dev.rs
+++ src-tauri/src/utils/open_dev.rs
@@ -4,8 +4,11 @@ use tauri::Manager;
 pub async fn open_dev(
     app: tauri::AppHandle
 ) -> Result<(), String> {
-    #[cfg(debug_assertions)]
-    let window = app.get_webview_window("main").unwrap();
-    window.open_devtools();
+    #[cfg(all(debug_assertions, target_os = "windows"))]
+    {
+        let window = app.get_webview_window("main").unwrap();
+        window.open_devtools();
+    }
+
     Ok(())
 }
