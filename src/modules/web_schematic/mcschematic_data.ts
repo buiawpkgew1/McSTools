37d025bb64cefce2ed71f7c998bebf996daecf07
guapi-exe <3051016381@qq.com>
2025-05-18 20:38:46 +0800

    修复调试模式-v0.4.5

--- src/modules/web_schematic/mcschematic_data.ts
+++ src/modules/web_schematic/mcschematic_data.ts
@@ -55,7 +55,9 @@ export const fetchMcSchematics = async (
         }
 
         const data = await response.json() as any[];
-
+        toast.info(`data:${data}`, {
+            timeout: 3000
+        });
         return data.map(item => ({
             ...item
         }));
